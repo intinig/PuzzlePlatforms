a2a7f4a9cec96eefd33d9c9be50cde71865cfedb
Giovanni Intini <intinig@gmail.com>
2018-12-15 21:30:54 +0000

    moving boxes

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform() {
+    PrimaryActorTick.bCanEverTick = true;
+    SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+
+    FVector Location = GetActorLocation();
+    Location += FVector(5 * DeltaTime, 0, 0);
+    SetActorLocation(Location);
+}
